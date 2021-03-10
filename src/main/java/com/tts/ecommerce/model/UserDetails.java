70a5d254d85b063be5f5ead7da7cfed88467411e
Elijah Munroe <elimunroe15@hotmail.com>
2021-03-10 10:35:03 -0500

    Remove Unnecessary User Details

--- src/main/java/com/tts/ecommerce/model/UserDetails.java
+++ src/main/java/com/tts/ecommerce/model/UserDetails.java
@@ -6,14 +6,5 @@ import javax.persistence.Transient;
 import java.util.Collection;
 
 public class UserDetails {
-    @Transient
-    private boolean accountNonExpired = true;
-    @Transient
-    private boolean accountNonLocked = true;
-    @Transient
-    private boolean credentialsNonExpired = true;
-    @Transient
-    private boolean enabled = true;
-    @Transient
-    private Collection<GrantedAuthority> authorities = null;
+
 }
