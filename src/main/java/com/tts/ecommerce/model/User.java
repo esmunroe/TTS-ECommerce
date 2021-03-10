eb3dc84e5158a962855506385e793e592308018f
Elijah Munroe <elimunroe15@hotmail.com>
2021-03-10 10:45:15 -0500

    Rewrite User Model to Cooperate w/ Project Code

--- src/main/java/com/tts/ecommerce/model/User.java
+++ src/main/java/com/tts/ecommerce/model/User.java
@@ -4,26 +4,40 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.validator.constraints.Length;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
 import java.util.Collection;
-import java.util.Date;
-import java.util.Set;
+import java.util.Map;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-// This is the user class that allows the user to be created.
 public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    private String password;
+
+    @ElementCollection
+    private Map<Product, Integer> cart;
+
+    public User(@NotEmpty String username, @NotEmpty String password, Map<Product, Integer> cart) {
+        this.username = username;
+        this.password = password;
+        this.cart = cart;
+    }
+
     @Transient
     private boolean accountNonExpired = true;
     @Transient
@@ -34,32 +48,4 @@ public class User implements UserDetails {
     private boolean enabled = true;
     @Transient
     private Collection<GrantedAuthority> authorities = null;
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "user_id")
-    private Long id;
-
-    @Email(message = "Please provide a valid email")
-    @NotEmpty(message = "Please provide an email")
-    private String email;
-
-    @Length(min = 3, message = "your username must have more then 3 letters")
-    @Length(max = 15, message = "Your username cannot have more than 15 characters")
-    @Pattern(regexp = "[^\\s]+", message = "Your username cannot contain spaces")
-    private String username;
-    @Length(min = 5, message = "Your password needs to be more than 5 characters")
-    private String password;
-    @Length(message = "Cant leave empty")
-    private String firstName;
-    @Length(message = "Cant leave empty")
-    private String lastName;
-    private int active;
-
-    @CreationTimestamp
-    private Date createdAt;
-
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles;
 }
